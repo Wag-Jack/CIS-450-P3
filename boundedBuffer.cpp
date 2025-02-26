905c850275b32e1c92a1184dbe66582b8e34b01d
Jack Wagner <wagjack@umich.edu>
2025-02-26 13:26:03 -0500

    expanded for multiple producers and consumers
    i love vectors

--- boundedBuffer.cpp
+++ boundedBuffer.cpp
@@ -20,9 +20,18 @@
 using namespace std;
 
 const int MAX = 100; // the maximum possible value of the internal counter for semaphores
+const int NUM_PROD = 3;
+const int NUM_CONS = 3;
 
-int buffer; // shared buffer, size = 1
+int buffer; // shared buffer, size = 1 /*Change buffer size to n*/
 int numIters;
+
+/*
+TODO:
+1. Allow multiple producers/consumers
+2. Change bugger size from 1 to n, n being a command line parameter
+3. Main thread creates 3 producers and 3 consumers
+*/
 
 // deposit 1, ..., numIters into the data buffer
 void Producer(counting_semaphore<MAX> &sem_empty, counting_semaphore<MAX> &sem_full)
@@ -51,24 +60,42 @@ void Consumer(counting_semaphore<MAX> &sem_empty, counting_semaphore<MAX> &sem_f
 }
 
 // main() -- read command line and create threads
+// Input: ./boundedBuffer (numIters) (buffer))
 int main(int argc, char *argv[])
 {
-    thread producer, consumer;
+    vector<thread> producer;
+    vector<thread> consumer;
 
-    if (argc < 2)
+    if (argc < 3)
     {
-        printf("Usage: boundedBuffer <Number of Iterations>\n");
+        printf("Usage: boundedBuffer <Number of Iterations> <Buffer size>\n");
         exit(0);
     }
     numIters = atoi(argv[1]);
+    buffer = atoi(argv[2]);
 
     counting_semaphore<MAX> sem_empty(1);
     counting_semaphore<MAX> sem_full(0);
 
-    producer = thread(Producer, ref(sem_empty), ref(sem_full));
-    consumer = thread(Consumer, ref(sem_empty), ref(sem_full));
+    //Create vector of NUM_PROD producer threads
+    for (int i = 0; i < NUM_PROD; i++) {
+        producer.push_back(thread(Producer, ref(sem_empty), ref(sem_full)));
+    }
 
-    producer.join();
-    consumer.join();
+    //Create vector of NUM_CONS consumer threads
+    for (int i = 0; i < NUM_CONS; i++) {
+        consumer.push_back(thread(Consumer, ref(sem_empty), ref(sem_full)));
+    }
+
+    //Execute all producer threads
+    for (int i = 0; i < producer.size(); i++) {
+        producer[i].join();
+    }
+
+    //Execute all consumer threads
+    for (int i = 0; i < consumer.size(); i++) {
+        consumer[i].join();
+    }
+
     exit(0);
 }
